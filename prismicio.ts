05b71c374684288503497ad632606c0cf5bcdee2
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2023-08-11 08:43:00 +0000

    tessssst

--- prismicio.ts
+++ prismicio.ts
@@ -33,7 +33,7 @@ export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
     fetchOptions:
       process.env.NODE_ENV === 'production'
         ? { next: { tags: ['prismic'] }, cache: 'force-cache' }
-        : { next: { revalidate: 10 } },
+        : { next: { revalidate: 5 } },
     ...config,
   })
 
